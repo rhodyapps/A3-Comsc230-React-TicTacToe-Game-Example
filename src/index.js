da682dd56699e8594aa77f9aecc8bb4fd1e4397f
tnewman <tnewman@meditech.com>
2020-09-15 15:09:18 -0400

    status msg who is next player

--- src/index.js
+++ src/index.js
@@ -90,8 +90,8 @@ function Square(props) {
   }
   
     render() {
-      const status = 'Next player: X';
-  
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
       return (
         <div>
           <div className="status">{status}</div>
