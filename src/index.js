3109394175344efc107e92bc3b92aa7c9d5e8ade
tnewman <tnewman@meditech.com>
2020-09-15 15:35:10 -0400

    winner

--- src/index.js
+++ src/index.js
@@ -4,35 +4,12 @@ import './index.css';
 
 
 /* 
-In this branch we will add the ability to have players take turns and the topic 'immutability'.
+In this branch we will add a helper function at the bottom of the file 
+to calculate if there is a winner. 
+The calculateWinner() function will be called from Board render().
 
-First, immutability: 
-There are generally two approaches to changing data. 
-The first approach is to mutate the data by directly changing the data’s values.
-The second approach is to replace the data with a new copy which has the desired 
-changes.
-
-Immutability makes complex features much easier to implement. Later in this tutorial,
-we will implement a “time travel” feature that allows us to review the tic-tac-toe game’s 
-history and “jump back” to previous moves. This functionality isn’t specific to games
-— an ability to undo and redo certain actions is a common requirement in applications. 
-Avoiding direct data mutation lets us keep previous versions of the game’s history intact, 
-and reuse them later.
-
-Detecting changes in mutable objects is difficult because they are modified directly.
- This detection requires the mutable object to be compared to previous copies of itself
-  and the entire object tree to be traversed.
-
-Detecting changes in immutable objects is considerably easier.
- If the immutable object that is being referenced is different than the previous one,
-  then the object has changed.
-
-The main benefit of immutability is that it helps you build pure components in React.
-Immutable data can easily determine if changes have been made, which helps to determine
-when a component requires re-rendering.
-
-Replace the previous version of the Square class with this function:
-We have changed this.props to props both times it appears.
+We also change the Board’s handleClick function to return early by 
+ignoring a click if someone has won the game or if a Square is already filled
 
 */
 
@@ -69,6 +46,9 @@ function Square(props) {
 
    handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     console.log("xIsNext: ",this.state.xIsNext);
      // Here we flip the boolean
@@ -90,7 +70,13 @@ function Square(props) {
   }
   
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
       return (
         <div>
@@ -138,3 +124,24 @@ function Square(props) {
     document.getElementById('root')
   );
   
+  // helper function to be called from Board's 'render' function
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
