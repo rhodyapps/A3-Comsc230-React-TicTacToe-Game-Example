ce1f771b86ae114115615d976f32933b063f7357
tnewman <tnewman@meditech.com>
2020-09-15 13:58:53 -0400

    handleclick

--- src/index.js
+++ src/index.js
@@ -2,23 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// Note that in this part of the program we have plain javascript
-/*  we can use the JS comment syntax for single 
-or multiline comments
-*/
+
+/* To collect data from multiple children, or to have two child components communicate
+ with each other, you need to declare the shared state in their parent component (Board) instead of in the child (Square)
+ The parent component can pass the state back down to the children by using props; 
+ this keeps the child components in sync with each other and with the parent component.
+
+In this branch we will add an array to the Board component to hold the state value of each Square.
+ */
 
 class Square extends React.Component {
    
-    /* 
-    React components can have state by setting this.state in their constructors.
-     this.state should be considered as private to a React component that it’s defined in.
-      Let’s store the current value of the Square in this.state, and change it when the Square is clicked.
-    
-    First, we’ll add a constructor to the class to initialize the state:
-     Note: In JavaScript classes, you need to always call super when defining the constructor of a subclass.
-       All React component classes that have a constructor should start with a super(props) call.
-       
-    */
+   
     
      constructor(props) {
         super(props);
@@ -52,11 +47,52 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      {//return <Square />;}
-      return <Square value={i} />;
-    }
-    }
+   
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+      }
+     /* When we play the game the Board array will look like this:
+     [
+        'O', null, 'X',
+        'X', 'X', 'O',
+        'O', null, null,
+      ]
+      */
+   
+    /*
+    We will now use the prop passing mechanism again. 
+    We will modify the Board to instruct each individual 
+    Square about its current value ('X', 'O', or null). 
+    We have already defined the squares array in the Board’s constructor,
+    and we will modify the Board’s renderSquare method to read from it.
+
+    so we add the following to renderSquare:
+    return <Square value={this.state.squares[i]}
+
+    Next, we need to change what happens when a Square is clicked. 
+    The Board component now maintains which squares are filled. 
+
+    We need to create a way for the Square to update the Board’s state. 
+    Since state is considered to be private to a component that defines it, 
+    we cannot update the Board’s state directly from Square.
+
+    Instead, we’ll pass down a function from the Board to the Square, 
+    and we’ll have Square call that function when a square is clicked.
+     We’ll change the renderSquare method in Board to call a function that
+     is passed down from the Board to the Square when a Square is clicked as follows:
+    */
+
+   renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
   
     render() {
       const status = 'Next player: X';
