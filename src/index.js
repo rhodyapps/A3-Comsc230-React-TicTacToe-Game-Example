2dd7ac6839c569fcb1e71de8b726ac23b9e9c0df
tnewman <tnewman@meditech.com>
2020-09-15 15:06:32 -0400

    xIsNext

--- src/index.js
+++ src/index.js
@@ -3,53 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-/* To collect data from multiple children, or to have two child components communicate
- with each other, you need to declare the shared state in their parent component (Board) instead of in the child (Square)
- The parent component can pass the state back down to the children by using props; 
- this keeps the child components in sync with each other and with the parent component.
+/* 
+In this branch we will add the ability to have players take turns and the topic 'immutability'.
 
-In this branch we will add an array to the Board component to hold the state value of each Square.
- 
-After these changes, we’re again able to click on the Squares to fill them, the same as we had before. 
-However, now the state is stored in the Board component instead of the individual Square components. 
-When the Board’s state changes, the Square components re-render automatically. 
-Keeping the state of all squares in the Board component will allow it to determine the winner in the future.
+First, immutability: 
+There are generally two approaches to changing data. 
+The first approach is to mutate the data by directly changing the data’s values.
+The second approach is to replace the data with a new copy which has the desired 
+changes.
+
+Immutability makes complex features much easier to implement. Later in this tutorial,
+we will implement a “time travel” feature that allows us to review the tic-tac-toe game’s 
+history and “jump back” to previous moves. This functionality isn’t specific to games
+— an ability to undo and redo certain actions is a common requirement in applications. 
+Avoiding direct data mutation lets us keep previous versions of the game’s history intact, 
+and reuse them later.
+
+Detecting changes in mutable objects is difficult because they are modified directly.
+ This detection requires the mutable object to be compared to previous copies of itself
+  and the entire object tree to be traversed.
+
+Detecting changes in immutable objects is considerably easier.
+ If the immutable object that is being referenced is different than the previous one,
+  then the object has changed.
+
+The main benefit of immutability is that it helps you build pure components in React.
+Immutable data can easily determine if changes have been made, which helps to determine
+when a component requires re-rendering.
+
+Replace the previous version of the Square class with this function:
+We have changed this.props to props both times it appears.
 
 */
 
-class Square extends React.Component {
-   
-   
-    
-     constructor(props) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-      }
-
-    render() {
-      return (
-        <button  className="square"
-        onClick={() => this.setState({value: 'X'})}>
-                {/* 
-                Replace this.props.value with this.state.value inside the <button> tag.
-                Replace the onClick={...} event handler with onClick={() => this.setState({value: 'X'})}.
-                Put the className and onClick props on separate lines for better readability.
-                 
-                By calling this.setState from an onClick handler in the Square’s render method, 
-                we tell React to re-render that Square whenever its <button> is clicked. 
-                 When you call setState in a component, React automatically 
-                 updates the child components inside of it too.
-                 */ }
-         {this.state.value}
-         {/* Use Chrome Developer tools > Console to see this.state.value as you click */}
-         {console.log(this.state.value)}
-         
-         
-        </button>
-      );
-    }
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
@@ -58,6 +50,7 @@ class Square extends React.Component {
         super(props);
         this.state = {
           squares: Array(9).fill(null),
+          xIsNext: true, // we add this boolean to have a way to tell who goes next
         };
       }
      /* When we play the game the Board array will look like this:
@@ -69,35 +62,21 @@ class Square extends React.Component {
       */
    
     /*
-    We will now use the prop passing mechanism again. 
-    We will modify the Board to instruct each individual 
-    Square about its current value ('X', 'O', or null). 
-    We have already defined the squares array in the Board’s constructor,
-    and we will modify the Board’s renderSquare method to read from it.
-
-    so we add the following to renderSquare:
-    return <Square value={this.state.squares[i]}
-
-    Next, we need to change what happens when a Square is clicked. 
-    The Board component now maintains which squares are filled. 
-
-    We need to create a way for the Square to update the Board’s state. 
-    Since state is considered to be private to a component that defines it, 
-    we cannot update the Board’s state directly from Square.
-
-    Instead, we’ll pass down a function from the Board to the Square, 
-    and we’ll have Square call that function when a square is clicked.
-     We’ll change the renderSquare method in Board to call a function that
-     is passed down from the Board to the Square when a Square is clicked as follows:
-    
-    Note how in handleClick, we call .slice() to create a copy of the squares array
-     to modify instead of modifying the existing array. 
+    Each time a player moves, xIsNext (a boolean) will be flipped to 
+    determine which player goes next and the game’s state will be saved.
+    We’ll update the Board’s handleClick function to flip the value of xIsNext
      */
 
    handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    console.log("xIsNext: ",this.state.xIsNext);
+     // Here we flip the boolean
+    this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+        // this is using the 'not' operator to set either X or O
+      });
   }
 
 
