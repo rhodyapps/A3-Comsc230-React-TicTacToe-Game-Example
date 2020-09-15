8a1fbd310485b21bb14496b5a12395ce69fe9254
tnewman <tnewman@meditech.com>
2020-09-15 16:44:52 -0400

    key added to list item

--- src/index.js
+++ src/index.js
@@ -4,70 +4,38 @@ import './index.css';
 
 
 /* 
-In this branch we will make it possible to “go back in time” to the previous moves
-in the game.
+/* 
+In this branch we will add keys to the dynamic list items.
+ Keys tell React about the identity of each component which allows 
+ React to maintain state between re-renders. If a component’s key changes, 
+ the component will be destroyed and re-created with a new state.steps were taken.
 
-If we mutated the squares array, implementing time travel would be very difficult.
+key is a special and reserved property in React (along with ref, a more advanced 
+feature). When an element is created, React extracts the key property and stores 
+the key directly on the returned element. Even though key may look like it belongs 
+in props, key cannot be referenced using this.props.key.
+ React automatically uses key to decide which components to update.
+  A component cannot inquire about its key.
 
-However, we used slice() to create a new copy of the squares array after every move,
- and treated it as immutable. This will allow us to store every past version of the
- squares array, and navigate between the turns that have already happened.
+It’s strongly recommended that you assign proper keys whenever you build dynamic lists.
+If you don’t have an appropriate key, you may want to consider restructuring your data 
+so that you do.
 
-We’ll store the past squares arrays in another array called history.
-The history array represents all board states, from the first to the last move, 
-and has a shape like this:
+If no key is specified, React will present a warning and use the array index as a key
+by default. Using the array index as a key is problematic when trying to re-order
+a list’s items or inserting/removing list items. 
+Explicitly passing key={i} silences the warning but has the same problems as array
+indices and is not recommended in most cases.
 
-history = [
-  // Before first move
-  {
-    squares: [
-      null, null, null,
-      null, null, null,
-      null, null, null,
-    ]
-  },
-  // After first move
-  {
-    squares: [
-      null, null, null,
-      null, 'X', null,
-      null, null, null,
-    ]
-  },
-  // After second move
-  {
-    squares: [
-      null, null, null,
-      null, 'X', null,
-      null, null, 'O',
-    ]
-  },
-  // ...
-]
+Keys do not need to be globally unique; 
+they only need to be unique between components and their siblings.
 
-Now we need to decide which component should own the history state.
-We’ll want the top-level Game component to display a list of past moves. 
-It will need access to the history to do that, so we will place the history
- state in the top-level Game component.
+In the tic-tac-toe game’s history, each past move has a unique ID associated with it:
+ it’s the sequential number of the move. The moves are never re-ordered, deleted, 
+ or inserted in the middle, so it’s safe to use the move index as a key.
 
-Placing the history state into the Game component lets us remove the squares 
-state from its child Board component. Just like we “lifted state up” from the 
-Square component into the Board component, we are now lifting it up from the Board
-into the top-level Game component. This gives the Game component full control over the Board’s data, and lets it instruct the Board to render previous turns from the history.
-
-First, we’ll set up the initial state for the Game component within its constructor.
-
-Next, we’ll have the Board component receive squares and onClick props from the
- Game component. Since we now have a single click handler in Board for many Squares,
-  we’ll need to pass the location of each Square into the onClick handler to indicate 
-  which Square was clicked. Here are the required steps to transform the Board 
-  component:
-
-    1. Delete the constructor in Board.
-    2. Replace this.state.squares[i] with this.props.squares[i] in Board’s renderSquare.
-    3. Replace this.handleClick(i) with this.props.onClick(i) in Board’s renderSquare.
-
-    In this branch the Board component has the code after these steps were taken.
+In the Game component’s render method, we can add the key as <li key={move}> 
+and React’s warning about keys should disappear.
 
 */
 
@@ -151,7 +119,7 @@ function Square(props) {
             'Go to move #' + move :
             'Go to game start';
           return (
-            <li>
+            <li key={move}>
               <button onClick={() => this.jumpTo(move)}>{desc}</button>
             </li>
           );
