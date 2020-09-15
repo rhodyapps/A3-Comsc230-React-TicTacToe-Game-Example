a9562e15f886ed69596f8e690f5f0ec083d2dcd0
tnewman <tnewman@meditech.com>
2020-09-15 16:27:45 -0400

    history buttons

--- src/index.js
+++ src/index.js
@@ -141,32 +141,44 @@ function Square(props) {
       }
 
 
-    render() {
+      render() {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+    
+        const moves = history.map((step, move) => {
+          const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+          return (
+            <li>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+          );
+        });
+    
         let status;
         if (winner) {
           status = 'Winner: ' + winner;
         } else {
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
-      
+    
         return (
-        <div className="game">
-         <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
+          <div className="game">
+            <div className="game-board">
+              <Board
+                squares={current.squares}
+                onClick={(i) => this.handleClick(i)}
+              />
+            </div>
+            <div className="game-info">
+              <div>{status}</div>
+              <ol>{moves}</ol>
+            </div>
           </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
+        );
+      }
   }
   
   // ========================================
